120f83501ad46e6efd38a87dd1058a68d87db656
rhk998 <130037816+rhk998@users.noreply.github.com>
2025-09-02 13:33:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+const applauseButton = document.getElementById('applause');
+const booButton = document.getElementById('boo');
+const gaspButton = document.getElementById('gasp');
+const tadaButton = document.getElementById('tada');
+const victoryButton = document.getElementById('victory');
+const wrongButton = document.getElementById('wrong');
+const stopButton = document.getElementById('stop');
+
+
+const applause = new Audio('sounds/applause.mp3');
+const boo = new Audio('sounds/boo.mp3');
+const gasp = new Audio('sounds/gasp.mp3');
+const tada = new Audio('sounds/tada.mp3');
+const victory = new Audio('sounds/victory.mp3');
+const wrong = new Audio('sounds/wrong.mp3');
+
+// Event listeners for button clicks to play sounds
+applauseButton.addEventListener('click', () => applause.play());
+booButton.addEventListener('click', () => boo.play());
+gaspButton.addEventListener('click', () => gasp.play());
+tadaButton.addEventListener('click', () => tada.play());
+victoryButton.addEventListener('click', () => victory.play());
+wrongButton.addEventListener('click', () => wrong.play());
+
+stopButton.addEventListener('click', stopSounds);
